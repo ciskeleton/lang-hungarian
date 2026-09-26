18696ff2dabac4ab3ce125419a9ea06166422961
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-09-26 10:24:32 +0100

    Squashing Commit!

---
+++ src/cookies_lang.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Cookies language file.
+ *
+ * Contains labels and messages used by the cookie-consent banner,
+ * cookie settings UI, and cookie policy helpers.
+ */
+
+$lang['cookies_consent_title'] = 'Sütiket használunk';
+$lang['cookies_consent_message'] = 'Szükséges sütiket használunk a webhely működésének biztosításához. Az Ön engedélyével elemzési és marketing sütiket is használunk, hogy megértsük, hogyan használják a látogatók a webhelyet, és javítsuk tartalmainkat.';
+$lang['cookies_consent_accept_all'] = 'Összes elfogadása';
+$lang['cookies_consent_accept_required'] = 'Csak szükséges';
+
+/**
+ * ---------------------------------------------------------------
+ * Cookie Categories
+ * ---------------------------------------------------------------
+ */
+
+$lang['cookies_category_necessary_title'] = 'Szükséges sütik';
+$lang['cookies_category_necessary_label'] = 'Mindig aktív';
+$lang['cookies_category_necessary_description'] = 'Ezek a sütik szükségesek a webhely megfelelő működéséhez. Olyan alapvető funkciókat támogatnak, mint a sütikhez való hozzájárulás, a nyelvi beállítások, a hitelesítés, a biztonság, a munkamenetek és a megbízható eszköz ellenőrzése. Mindig aktívak, mert szükségesek a webhely működéséhez.';
+
+$lang['cookies_category_analytics_title'] = 'Elemzési sütik';
+$lang['cookies_category_analytics_label'] = 'Opcionális';
+$lang['cookies_category_analytics_description'] = 'Ezek a sütik segítenek megérteni, hogyan használják a látogatók a webhelyet, például mely oldalakat látogatják meg, és hogyan lépnek kapcsolatba a felhasználók a webhellyel. Ezek az információk segítenek javítani a teljesítményt, a használhatóságot és a tartalmat. Az elemzési sütik csak akkor használatosak, ha a webhely rendszergazdája engedélyezte őket, és a látogató elfogadta azokat.';
+
+$lang['cookies_category_marketing_title'] = 'Marketing sütik';
+$lang['cookies_category_marketing_label'] = 'Opcionális';
+$lang['cookies_category_marketing_description'] = 'Ezek a sütik kampányok mérésére, hirdetések személyre szabására vagy marketing szolgáltatások, például hirdetési pixelek integrálására használhatók. Ezeket a webhely rendszergazdája által konfigurált harmadik féltől származó szolgáltatások állíthatják be, és csak akkor használatosak, ha a látogató elfogadta azokat.';
---
+++ src/info.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * CiSkeleton Language Metadata - Hungarian
+ *
+ * @package     CiSkeleton\Core
+ * @subpackage  Language
+ * @author      Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright   Copyright (c) 2018-present, Kader Bouyakoub
+ */
+
+return [
+	'name'       => 'Magyar',
+	'name_en'    => 'Hungarian',
+	'locale'     => 'hu_HU',
+	'tag'        => 'hu-HU',
+	'direction'  => 'ltr',
+	'code'       => 'hu',
+	'flag'       => 'hu',
+	'version'    => '1.0.0',
+	'version_id' => 10000,
+
+	// Localized language names.
+	'localized_names' => [
+		'arabic'     => 'Arab',
+		'armenian'   => 'Örmény',
+		'bengali'    => 'Bengáli',
+		'brazilian'  => 'Portugál (Brazil)',
+		'bulgarian'  => 'Bolgár',
+		'catalan'    => 'Katalán',
+		'chinese'    => 'Kínai (Egyszerűsített)',
+		'croatian'   => 'Horvát',
+		'czech'      => 'Cseh',
+		'danish'     => 'Dán',
+		'dutch'      => 'Holland',
+		'english'    => 'Angol',
+		'estonian'   => 'Észt',
+		'filipino'   => 'Filippínó',
+		'finnish'    => 'Finn',
+		'french'     => 'Francia',
+		'georgian'   => 'Grúz',
+		'german'     => 'Német',
+		'greek'      => 'Görög',
+		'hindi'      => 'Hindi',
+		'hungarian'  => 'Magyar',
+		'indonesian' => 'Indonéz',
+		'italian'    => 'Olasz',
+		'japanese'   => 'Japán',
+		'korean'     => 'Koreai',
+		'latvian'    => 'Lett',
+		'lithuanian' => 'Litván',
+		'malay'      => 'Maláj',
+		'norwegian'  => 'Norvég',
+		'persian'    => 'Perzsa',
+		'polish'     => 'Lengyel',
+		'portuguese' => 'Portugál',
+		'romanian'   => 'Román',
+		'russian'    => 'Orosz',
+		'serbian'    => 'Szerb',
+		'slovak'     => 'Szlovák',
+		'slovenian'  => 'Szlovén',
+		'spanish'    => 'Spanyol',
+		'swahili'    => 'Szuahéli',
+		'swedish'    => 'Svéd',
+		'taiwanese'  => 'Kínai (Hagyományos)',
+		'tamil'      => 'Tamil',
+		'thai'       => 'Thai',
+		'turkish'    => 'Török',
+		'ukrainian'  => 'Ukrán',
+		'urdu'       => 'Urdu',
+		'vietnamese' => 'Vietnámi',
+	],
+];
